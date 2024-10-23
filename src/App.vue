457576a62c74e4e8d3edc3ccbfdcee568e77661c
DavChaxalyan <dchaxalyan@gmail.com>
2024-10-23 16:07:09 +0400

    create medlink community section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
+import MedlinkCommunity from './components/Medlink Community/MedlinkCommunity.vue';
 </script>
 
 <template>
-
+    <MedlinkCommunity />
 </template>
 
 <style scoped>
