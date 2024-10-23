78a49de7568ba791a803562f95dedc7bc62d4beb
Levon7878 <lyovbabagulyan@gmail.com>
2024-10-23 14:08:30 +0400

    add header images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './index.html',              // Убедись, что путь к index.html указан правильно
-    './src/**/*.{vue,js,ts,jsx,tsx}', // Убедись, что указаны все файлы, содержащие Tailwind классы
+    './index.html',              
+    './src/**/*.{vue,js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {},
