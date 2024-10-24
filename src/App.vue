e214ebb128e9f1ca7a3447b3a30813360d5f1ee6
Levon7878 <lyovbabagulyan@gmail.com>
2024-10-24 16:54:57 +0400

    design [200~Case Studies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MedlinkCommunity from './components/Medlink Community/MedlinkCommunity.vue';
 import Header from "./components/Header/Header.vue";
+import CaseStudies from './components/CaseStudies/CaseStudies.vue';
 </script>
 
 <template>
     <Header />
     <MedlinkCommunity />
+    <CaseStudies />
 </template>
 
 <style scoped>
---
+++ src/components/CaseStudies/CaseStudies.vue
@@ -0,0 +1,199 @@
+<template >
+  <div class="mt-150px">
+    <section class="bg-[#E4ECF7] pl-[134px] pr-[134px] mt-[150px]">
+      <div class="pt-[80px] flex gap-[134px]">
+        <div class="flex items-center flex-wrap gap-[16px] max-w-[1140px]">
+          <div class="py-2 px-4 bg-[#BAD8F14D] rounded-[27px]">
+            <p class="font-medium text-[24px] leading-[28.8px] text-[#1F2933]">
+              02
+            </p>
+          </div>
+          <h2 class="text-[64px] leading-[64px] text-[#1F2933]">
+            Case Studies
+          </h2>
+        </div>
+        <p
+          class="text-[20px] leading-[24px] font-medium max-w-[463px] text-[#1F2933A3]"
+        >
+          Explore many case studies from doctors and patients, witness the
+          MedLink Community in action.
+        </p>
+      </div>
+      <div class="relative">
+        <div
+          class="max-w-[1140px] flex gap-[4px] bg-[#FFFFFF33] rounded-[110px] items-center mt-[40px]"
+        >
+          <div
+            class="pt-6 pr-8 pb-6 pl-8 bg-[#DAFF98] rounded-[40px] max-w-[83px] max-h-[70px]"
+          >
+            <p class="text-[#398B39]">All</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p>Cancer</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p class="w-[95px]">Chronic Pain</p>
+          </div>
+          <div class="w-[215px] pt-6 pr-8 pb-6 pl-8">
+            <p class="w-[151px]">High Blood Pressure</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p>Diabetes</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p class="w-[83px]">Skin issues</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p>Depression</p>
+          </div>
+          <div class="max-w-[190px] pt-6 pr-8 pb-6 pl-8">
+            <p class="w-[134px]">Suicidal Thoughts</p>
+          </div>
+        </div>
+        <div
+          class="w-[54px] h-[54px] bg-[#FFFFFF] rounded-[40px] absolute top-[27px] right-[28%] bottom-[15px] flex justify-center items-center"
+        >
+          <img src="../../assets/images/rigth-icon.png" alt="" />
+        </div>
+      </div>
+      <div class="mt-[40px]">
+        <div class="flex gap-[20px]">
+          <div class="max-w-[560px] flex flex-col gap-[40px]">
+            <div class="relative">
+              <img
+                class="max-w-[560px]"
+                src="../../assets/images/nurse.png"
+                alt="Nurse"
+              />
+              <div
+                class="rotate-[-90deg] absolute left-[370px] right-[-308px] top-[208px] max-w-[363px] h-[24px]"
+              >
+                <img src="../../assets/images/clip-path-community.png" alt="" />
+              </div>
+            </div>
+            <div>
+              <h4 class="font-medium text-[24px] leading-[28.8px]">
+                Overcoming Cancer: Emily's Triumph
+              </h4>
+              <div class="overflow-hidden whitespace-nowrap overflow-ellipsis">
+                <p
+                  class="mt-[16px] text-[20px] text-medium leading-[24px] text-[#1F2933A3] w-[560px] overflow-ellipsis"
+                >
+                  Follow Emily's inspiring journey through diagnosis, treatment,
+                  and resilience as she shares her strategies for maintaining
+                  hope and strength.
+                </p>
+              </div>
+              <div class="flex items-center mt-[16px] gap-[8px]">
+                <a> Watch now </a>
+                <div class="">
+                  <img
+                    class="w-[5px] h-[10px]"
+                    src="../../assets/images/mini-right-icon.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-[16px]">
+            <div
+              class="max-w-[554px] flex border border-[#1F293314] rounded-[40px] p-[24px]"
+            >
+              <div class="">
+                <h3
+                  class="text-[#1F2933] text-[24px] leading-[28.8px] font-medium max-w-[222px]"
+                >
+                  From Darkness to Light: Someone’s Battle with Depression
+                </h3>
+                <p
+                  class="w-[100px] max-w-[222px] overflow-hidden whitespace-nowrap overflow-ellipsis"
+                >
+                  Join Sarah as she opens up about her battle with depression,
+                  offering insights into the healing power of community and
+                  self-care.
+                </p>
+                <div class="flex items-center gap-[8px]">
+                  <a> Watch now </a>
+
+                  <img
+                    class="w-[5px] h-[10px]"
+                    src="../../assets/images/mini-right-icon.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div>
+                <img src="../../assets/images/nurse-2.png" alt="" />
+              </div>
+            </div>
+            <div
+              class="max-w-[554px] flex flex-col border border-[#1F293314] rounded-[40px] p-[24px]"
+            >
+              <div class="flex gap-[16px] items-start">
+                <img src="../../assets/images/microphone-01.png" alt="" />
+                <h3 class="text-[24px] font-medium leading-[28.8px]">
+                  What did you try that didn’t help you better?
+                </h3>
+              </div>
+              <div>
+                <div
+                  class="w-[24px] h-[24px] bg-[#398B39] flex items-center justify-center rounded-[80px]"
+                >
+                  <img
+                    class="w-[9px] h-[12px]"
+                    src="../../assets/images/solid-play.png"
+                    alt=""
+                  />
+                </div>
+                <div class="flex items-center mt-[16px] gap-[8px]">
+                  <a> Listen now </a>
+
+                  <img
+                    class="w-[5px] h-[10px]"
+                    src="../../assets/images/mini-right-icon.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div
+              class="max-w-[554px] flex flex-col gap-[16px] border border-[#1F293314] rounded-[40px] p-[24px]"
+            >
+              <div class="flex items-start gap-[16px]">
+                <img src="../../assets/images/font-size 01.png" alt="" />
+                <h3 class="w-[448px]">
+                  What symptoms did you have? What was the western medical
+                  diagnosis and/or the eastern medicine diagnosis?
+                </h3>
+              </div>
+              <p
+                class="text-[#1F2933A3] font-medium text-[20px] rounded-[24px]"
+              >
+                Join Sarah as she opens up about her battle with depression,
+                offering insights into the healing power of community and
+                self-care.
+              </p>
+              <div class="flex items-center mt-[16px] gap-[8px]">
+                <a> Listen now </a>
+
+                <img
+                  class="w-[5px] h-[10px]"
+                  src="../../assets/images/mini-right-icon.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center mt-[40px] max-w-[1140px]">
+        <button class="pt-[32px] pb-[32px] pl-[64px] pr-[64px] bg-[#1F2933] rounded-[40px] text-[#FFFFFF]">View All Conditions</button>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+</script>
+<style scoped >
+</style>
