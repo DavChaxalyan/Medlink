4c5a7ff75fc06fd5ebab7bbc4a515b4dccf2a97d
Davit <dchaxalyan@gmail.com>
2024-10-24 15:53:54 +0400

    create video showcase section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MedlinkCommunity from './components/Medlink Community/MedlinkCommunity.vue';
 import Header from "./components/Header/Header.vue";
+import VideoShowcase from './components/Video Showcase/VideoShowcase.vue';
 </script>
 
 <template>
     <Header />
     <MedlinkCommunity />
+    <VideoShowcase />
 </template>
 
 <style scoped>
