d09378750fde55492ae56b86e1176bbe05be852f
Davit <dchaxalyan@gmail.com>
2024-10-24 18:22:50 +0400

    FAQs section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MedlinkCommunity from './components/Medlink Community/MedlinkCommunity.vue';
 import Header from "./components/Header/Header.vue";
+import FAQs from "./components/FAQs/FAQs.vue";
 </script>
 
 <template>
     <Header />
     <MedlinkCommunity />
+    <FAQs />
 </template>
 
 <style scoped>
