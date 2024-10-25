c9ec42c7940ff1231eaf77f22a61ff69605569e0
Levon7878 <lyovbabagulyan@gmail.com>
2024-10-25 14:51:23 +0400

    latest posts slide

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MedlinkCommunity from './components/Medlink Community/MedlinkCommunity.vue';
 import Header from "./components/Header/Header.vue";
+import LatestPosts from './components/LatestPosts/LatestPosts.vue'
 </script>
 
 <template>
     <Header />
     <MedlinkCommunity />
+    <LatestPosts />
 </template>
 
 <style scoped>
