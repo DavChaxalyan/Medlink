a0d81581a79e7a8509795f6568134de2f3e99089
DavChaxalyan <dchaxalyan@gmail.com>
2024-10-29 15:21:02 +0400

    create Breast Cancer page

---
+++ src/pages/Breast Cancer/BreastCancer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Header />
+        
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import Header from "../../components/Header/Header.vue";
+import Footer from '../../components/Footer/Footer.vue';
+
+</script>
+
+<style scoped></style>
