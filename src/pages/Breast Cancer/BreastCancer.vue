7d32d16e329966eed47691e87a7d8676bf58a6f1
DavChaxalyan <dchaxalyan@gmail.com>
2024-10-29 16:35:32 +0400

    create Breast Cancer Page

--- src/pages/Breast Cancer/BreastCancer.vue
+++ src/pages/Breast Cancer/BreastCancer.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
         <Header />
-        
+        <BreastCancerTables />
+        <TextContainer />
         <Footer />
     </div>
 </template>
 
 <script setup>
 import Header from "../../components/Header/Header.vue";
+import BreastCancerTables from "../../components/CaseStudiesPageTables/BreastCancerTables.vue";
+import TextContainer from "../../components/Text Container/TextContainer.vue";
 import Footer from '../../components/Footer/Footer.vue';
 
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/Home/Home.vue';
 import CaseStudies from '../pages/Case Studies/CaseStudies.vue';
+import BreastCancer from '../pages/Breast Cancer/BreastCancer.vue';
 
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/case-studies', component: CaseStudies },
+    { path: '/breast-cancer', component: BreastCancer },
 ];
 
 const router = createRouter({
