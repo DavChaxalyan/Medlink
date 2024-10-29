7076a7147a0636a3ce4385a12fd44c969209eaab
Levon7878 <lyovbabagulyan@gmail.com>
2024-10-29 16:52:00 +0400

    create Header Breast Page

--- src/components/Video Showcase/VideoShowcase.vue
+++ src/components/Video Showcase/VideoShowcase.vue
@@ -13,6 +13,7 @@
             </div>
         </div>
         <VideoShowcaseSlider />
+        
     </div>
 </template>
 
--- src/pages/Breast Cancer/BreastCancer.vue
+++ src/pages/Breast Cancer/BreastCancer.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
         <Header />
-        
+        <ParticipantSummary/>
         <Footer />
     </div>
 </template>
 
 <script setup>
+import ParticipantSummary from "../../components/ParticipantSummary/ParticipantSummary.vue";
 import Header from "../../components/Header/Header.vue";
 import Footer from '../../components/Footer/Footer.vue';
 
